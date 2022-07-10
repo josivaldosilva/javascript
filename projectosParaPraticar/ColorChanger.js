1727f9f0aeac7465496e9147e80c1094700e010b
Josivaldo Silva <josivaldslv81@gmail.com>
2022-07-10 01:39:00 +0100

    Meu primeiro projecto JavaScript

---
+++ projectosParaPraticar/ColorChanger.js
@@ -0,0 +1,24 @@
+function red() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = ("red");
+
+    let text = document.getElementById("paragraph").innerHTML = "Background Color : Red";
+    
+};
+
+/*var btnred = document.getElementById("btnred");
+
+btnred.addEventListener("click", red());*/
+
+
+function blue() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = ("blue");
+
+    let text = document.getElementById("paragraph").innerHTML = "Background Color :  Blue";
+    
+}
+
+/*let btnblue = document.getElementById("btnblue");
+
+btnblue.addEventListener('click', blue());*/
