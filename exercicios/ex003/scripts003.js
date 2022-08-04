d59c4a9a035356152e801a59feea278abb06a5d6
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-04 19:51:44 +0100

    Objectos no JavaScript

--- exercicios/ex003/scripts003.js
+++ exercicios/ex003/scripts003.js
@@ -1,6 +1,7 @@
 // O tipos primitivos no JavaScript são pbasicamente dois: Tipos de referência e os tipos de Valores.
 
-// Referências:  Strings; Number; Boolean; Undefined e Null.
+//1º Referências:  Strings; Number; Boolean; Undefined e Null.
+
 
 let name = "Josivaldo"; // string literal
 
@@ -10,4 +11,5 @@ let approved = true; // boolean
 
 let surName; //Undefined
 
-let userName = null; // mais utilizao quando se quer redefinir um valor
+let userName = null; // mais utilizado quando se quer redefinir um valor.
+
---
+++ exercicios/ex004/scripts004.js
@@ -0,0 +1,17 @@
+
+//Objecto - Basicamente junta ou agrega valores em um lugar só.
+
+// Obejcto é definido no JavaScript a seguir a abrir colchetes e fecha colchetes e os seus e chaves valores são separado po uma virgula (,)
+
+let pessoa = {
+
+//   key      value
+    nome: "Josivaldo",
+    sobreNome: "da Silva",
+    altura: 172,
+    idade: 23,
+    raca: "Negro",
+    corCabelo: "Preto"
+};
+
+console.log(pessoa.idade);
