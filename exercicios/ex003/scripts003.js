c75a621779f6ccd9f4c0a7f6fe2beb9cfa75a4a1
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-04 19:23:12 +0100

    Tipos primitivos no JAvaScript

---
+++ exercicios/ex003/scripts003.js
@@ -0,0 +1,13 @@
+// O tipos primitivos no JavaScript são pbasicamente dois: Tipos de referência e os tipos de Valores.
+
+// Referências:  Strings; Number; Boolean; Undefined e Null.
+
+let name = "Josivaldo"; // string literal
+
+let age = 23; // number literal
+
+let approved = true; // boolean
+
+let surName; //Undefined
+
+let userName = null; // mais utilizao quando se quer redefinir um valor
