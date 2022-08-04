2da26502bb4c0c92697febf72c93fc540f3cce9b
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-04 20:07:15 +0100

    Arrays em JavaScript

---
+++ exercicios/ex005/scripts005.js
@@ -0,0 +1,10 @@
+// Arrays - É um conjunto de dados que pode ser acessado atráves do índice.
+
+// Podemos adicionar qualquer valor em um array, strings, numbers, boolean, object
+
+
+let family = ["Regina",26, true, 'Zezinho', 23, 21, 17, 13, 11, {nome: "Zezinho", idade: 23}];
+
+console.log(family[9].nome + " " + family[9].idade );
+
+console.log(family.length);
