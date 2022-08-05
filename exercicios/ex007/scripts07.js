2922c03b88f9c71afec0e6f809836c468b27557d
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-05 04:16:49 +0100

    Tipos de Funções

---
+++ exercicios/ex007/scripts07.js
@@ -0,0 +1,20 @@
+
+
+// 1º - Função que realiza uma tarefa e não devolve nada
+
+function dizerOla() {
+    console.log('Olá Mundo!');
+};
+
+dizerOla();
+
+
+//2º - Função que retorna valores depois de ser executada
+
+function multiplcarPor2(valor) {
+    return valor * 2
+};
+
+let resultado = multiplcarPor2(5);
+
+console.log(resultado);
