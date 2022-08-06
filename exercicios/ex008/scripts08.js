abde42bbf3020d644371066dc189f043cbd4be85
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-06 18:19:34 +0100

    Operadores de comparação, Lógicos, Ternário e etc

---
+++ exercicios/ex008/scripts08.js
@@ -0,0 +1,98 @@
+
+
+// Operadores Aritméticos(Matemáticos)
+// Operadores Aritméticos de Incremento (+1) e de Decremento (-1)
+// Operadores de Atribuição
+// Operadores de Compraração
+// Operadores Lógicos 
+// Operadores Bitwise
+
+/* Operadores Aritméticos (Matemáticos)
+
++ -> mais (soma);
+- -> menos (subtração); 
+* -> multiplicação; 
+/ -> Divisão; 
+** -> Exponênciação
+
+*/
+/*
+let salario = 100;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** salario);
+
+
+// Operadores de Aritméticos de Incrementação e de Decrementação
+
+let age = 23;
+
+// Incremento (++)
+console.log(++age);
+
+//Decremento (--)
+
+console.log(--age);
+
+
+
+// Operadores de atribuição no JavaScript (=)
+
+
+//                Op.Atrib
+let valorTecladoGamer = 1500;
+
+console.log(valorTecladoGamer);
+
+//valorTecladoGamer = valorTecladoGamer + valorTecladoGamer;
+
+console.log(valorTecladoGamer);
+
+// Simplificando
+valorTecladoGamer += valorTecladoGamer;
+
+console.log(valorTecladoGamer);
+
+
+
+// Operadores de igualdade no JavaScript (==)
+
+// Igualdade estrita (===)
+// Muito recomendado
+
+console.log(1 === 1);
+console.log(1 === '1');
+console.log(2 === 1);
+
+// Igualdade (==) 
+// Não muito recomendado
+
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(1 == '2');
+console.log(2 == 'two');
+
+
+
+// Operador Ternário
+
+
+let points = 110;
+let types = points > 100 ? "premium" : "Comum";
+console.log(types);
+
+*/
+
+// Operadores Lógicos no JavaScript
+
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+//Operador lógico E (&&)
+// retorna true se as udas consdiçoes a serem comparadas forem true
+
+console.log(true && true);
