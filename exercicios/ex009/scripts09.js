a8f084e17413e47d82f5833cfb41f432d35af5e9
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-07 23:31:22 +0100

    Comparações não Booleanas

---
+++ exercicios/ex009/scripts09.js
@@ -0,0 +1,19 @@
+// Falsy 
+// Valores falsy
+
+// undefined
+// null
+// ''
+// 0 
+// false
+// NaN
+
+
+// Truthy
+// Valores truthy
+
+let customizedColor = 'Red';
+let standardColor = "Blue";
+let profileColor = customizedColor || standardColor;
+
+console.log(profileColor);
