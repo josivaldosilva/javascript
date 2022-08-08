f69b3ac717bf25963b102023b806808d7f94bfde
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-08 02:41:27 +0100

    Laço/Loop de repetição while (enquanto)

---
+++ exercicios/ex013/scripts013.js
@@ -0,0 +1,24 @@
+// Laço/Loop de repetição while (enquanto)
+
+
+/*// Para utilizar o laço/loop while tem que:
+1º Definir a variável
+2º criar o loop white com a condição e o código a ser executado
+3º DDepois incrementar(I++)  ou decrementar(i--)
+
+
+let i; --> variável declarada
+
+while (condição) {
+    código a ser executado
+    i++; /i--
+}
+*/
+
+let i =5;
+
+while(i >= 1) {
+    console.log(i);
+    i--;
+}
+
