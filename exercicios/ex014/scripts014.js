f484ee3c9858cf7803be574b7543719a9722624b
Josivaldo Silva <josivaldslv81@gmail.com>
2022-08-08 12:39:52 +0100

    Laço/Loop de repetição for in

---
+++ exercicios/ex014/scripts014.js
@@ -0,0 +1,9 @@
+// Laço/Loop de repetição do while (faça enquanto)
+
+// A diferença entre o loop while e do while é que diferente do loop while o do while primeiro executa o código só depois é que verfificar a condição
+
+let i = 0;
+do {
+    console.log("Olá mundo", i);
+    i++;
+}while (i < 10);
